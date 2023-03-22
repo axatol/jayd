9c8c76474f7ce7f47f36cfb9972c8b7e8201dfeb
Hans Song <hans.m.song@gmail.com>
2023-03-22 19:55:54 +1000

    sort config kinda alphabetically

--- config/config.go
+++ config/config.go
@@ -10,48 +10,61 @@ import (
 )
 
 var (
-	envFile, _                = godotenv.Read()
-	Debug                     = envValueBool("DEBUG", false)
-	ServerBackupFile          = envValue("SERVER_BACKUP_FILE", "/data/cache.json")
-	ServerAddress             = envValue("SERVER_ADDRESS", ":8000")
-	ServerCORSList            = envValue("SERVER_CORS_LIST", "")
+	envFile, _ = godotenv.Read()
+	Debug      = envValueBool("DEBUG", false)
+
+	Auth0Enabled  = envValueBool("AUTH0_ENABLED", true)
+	Auth0Domain   = envValue("AUTH0_DOMAIN", "")
+	Auth0Audience = envValue("AUTH0_AUDIENCE", "")
+
 	DownloaderExecutable      = envValue("DOWNLOADER_EXECUTABLE", "yt-dlp")
 	DownloaderOutputDirectory = envValue("DOWNLOADER_OUTPUT_DIR", "/data/output")
 	DownloaderCacheDirectory  = envValue("DOWNLOADER_CACHE_DIR", "/data/cache")
 	DownloaderRetries         = envValueInt("DOWNLOADER_RETRIES", 1)
 	DownloaderConcurrency     = envValueInt("DOWNLOADER_CONCURRENCY", 1)
-	YoutubeAPIKey             = envValue("YOUTUBE_API_KEY")
-	Auth0Enabled              = envValueBool("AUTH0_ENABLED", true)
-	Auth0Domain               = envValue("AUTH0_DOMAIN", "")
-	Auth0Audience             = envValue("AUTH0_AUDIENCE", "")
-	WebDirectory              = envValue("WEB_DIRECTORY", "/web")
-	StorageEnabled            = envValueBool("STORAGE_ENABLED", true)
-	StorageEndpoint           = envValue("STORAGE_ENDPOINT", "")
-	StorageBucketName         = envValue("STORAGE_BUCKET_NAME", "jayd")
-	StorageAccessKeyID        = envValue("STORAGE_ACCESS_KEY_ID", "")
-	StorageSecretAccessKey    = envValue("STORAGE_SECRET_ACCESS_KEY", "")
+
+	ServerBackupFile = envValue("SERVER_BACKUP_FILE", "/data/cache.json")
+	ServerAddress    = envValue("SERVER_ADDRESS", ":8000")
+	ServerCORSList   = envValue("SERVER_CORS_LIST", "")
+
+	StorageEnabled         = envValueBool("STORAGE_ENABLED", true)
+	StorageEndpoint        = envValue("STORAGE_ENDPOINT", "")
+	StorageBucketName      = envValue("STORAGE_BUCKET_NAME", "jayd")
+	StorageAccessKeyID     = envValue("STORAGE_ACCESS_KEY_ID", "")
+	StorageSecretAccessKey = envValue("STORAGE_SECRET_ACCESS_KEY", "")
+
+	WebDirectory = envValue("WEB_DIRECTORY", "/web")
+
+	YoutubeAPIKey = envValue("YOUTUBE_API_KEY")
 )
 
 func init() {
 	flag.BoolVar(&Debug, "debug", Debug, "enable debug mode")
-	flag.StringVar(&ServerBackupFile, "server-backup-file", ServerBackupFile, "server backup file")
-	flag.StringVar(&ServerAddress, "server-address", ServerAddress, "enable debug mode")
-	flag.StringVar(&ServerCORSList, "server-cors-list", ServerCORSList, "server cors list")
+
+	flag.BoolVar(&Auth0Enabled, "auth0-enabled", Auth0Enabled, "auth0 enabled")
+	flag.StringVar(&Auth0Domain, "auth0-domain", Auth0Domain, "auth0 domain")
+	flag.StringVar(&Auth0Audience, "auth0-audience", Auth0Audience, "auth0 audience")
+
 	flag.StringVar(&DownloaderExecutable, "downloader-executable", DownloaderExecutable, "downloader executable")
 	flag.StringVar(&DownloaderOutputDirectory, "downloader-output-directory", DownloaderOutputDirectory, "downloader output directory")
 	flag.StringVar(&DownloaderCacheDirectory, "downloader-cache-directory", DownloaderCacheDirectory, "downloader cache directory")
 	flag.IntVar(&DownloaderRetries, "downloader-retries", DownloaderRetries, "downloader retries")
 	flag.IntVar(&DownloaderConcurrency, "downloader-concurrency", DownloaderConcurrency, "downloader concurrency")
-	flag.StringVar(&YoutubeAPIKey, "youtube-api-key", YoutubeAPIKey, "youtube api key")
-	flag.BoolVar(&Auth0Enabled, "auth0-enabled", Auth0Enabled, "auth0 enabled")
-	flag.StringVar(&Auth0Domain, "auth0-domain", Auth0Domain, "auth0 domain")
-	flag.StringVar(&Auth0Audience, "auth0-audience", Auth0Audience, "auth0 audience")
-	flag.StringVar(&WebDirectory, "web-directory", WebDirectory, "web directory")
+
+	flag.StringVar(&ServerBackupFile, "server-backup-file", ServerBackupFile, "server backup file")
+	flag.StringVar(&ServerAddress, "server-address", ServerAddress, "enable debug mode")
+	flag.StringVar(&ServerCORSList, "server-cors-list", ServerCORSList, "server cors list")
+
 	flag.BoolVar(&StorageEnabled, "storage-enabled", StorageEnabled, "storage enabled")
 	flag.StringVar(&StorageEndpoint, "storage-endpoint", StorageEndpoint, "storage endpoint")
 	flag.StringVar(&StorageBucketName, "storage-bucket-name", StorageBucketName, "storage bucket name")
 	flag.StringVar(&StorageAccessKeyID, "storage-access-key-id", StorageAccessKeyID, "storage access key id")
 	flag.StringVar(&StorageSecretAccessKey, "storage-secret-access-key", StorageSecretAccessKey, "storage secret access key")
+
+	flag.StringVar(&WebDirectory, "web-directory", WebDirectory, "web directory")
+
+	flag.StringVar(&YoutubeAPIKey, "youtube-api-key", YoutubeAPIKey, "youtube api key")
+
 	flag.Parse()
 
 	if Debug {
